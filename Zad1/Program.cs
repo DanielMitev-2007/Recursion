efe5681e967aa844f8a7b6446f11f7f1aa249e4b
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-02-13 18:26:54 +0200

    Add files via upload

---
+++ Zad1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int number = int.Parse(Console.ReadLine());
+            int sum = SumOfDigits(number);
+            Console.WriteLine($"The sum is : {sum} ");
+        }
+        static int SumOfDigits(int n)
+        {
+            if (n < 0) n = -n;
+            if(n == 0) return 0;
+            return (n%10) + SumOfDigits(n/10);
+        }
+    }
+}
