ffa22a04c65aa18e850622601ff6a57586d769e9
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-02-13 18:25:05 +0200

    Add files via upload

---
+++ zaddddd2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace zaddddd2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static bool isPowerOfTwo(int n)
+            {
+                if (n <= 0)
+                    return false;
+                if (n == 1)
+                    return true;
+                if(n % 2 !=0)
+                    return false;
+                return isPowerOfTwo(n / 2);
+            }
+            Console.Write("Enter number: ");
+            int number = int .Parse(Console.ReadLine());
+            Console.WriteLine("Result: " + isPowerOfTwo(number));
+            Console.ReadKey();
+        }
+    }
+}
